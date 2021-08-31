dabddec942cf3b7cca4339c30ee7a23cec43faad
Jon Rutter <contact@jonrutter.com>
2021-08-31 14:38:30 -0400

    Install and configure tailwind css and peer dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,22 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "Blogr Landing Page",
+    siteUrl: 'https://www.yourdomain.tld',
+    title: 'Blogr Landing Page',
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `src/images/icon.png`,
+      },
+    },
+    'gatsby-plugin-root-import',
+    'gatsby-plugin-postcss',
+  ],
 };
