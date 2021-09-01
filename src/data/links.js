67f5ba1412500ef2845204f0baa306535a31e069
Jon Rutter <contact@jonrutter.com>
2021-09-01 14:35:09 -0400

    Added data file

---
+++ src/data/links.js
@@ -0,0 +1,65 @@
+export const links = [
+  {
+    title: 'Product',
+    links: [
+      {
+        title: 'Overview',
+        href: '/',
+      },
+      {
+        title: 'Pricing',
+        href: '/',
+      },
+      {
+        title: 'Marketplace',
+        href: '/',
+      },
+      {
+        title: 'Features',
+        href: '/',
+      },
+      {
+        title: 'Integrations',
+        href: '/',
+      },
+    ],
+  },
+  {
+    title: 'Company',
+    links: [
+      {
+        title: 'About',
+        href: '/',
+      },
+      {
+        title: 'Team',
+        href: '/',
+      },
+      {
+        title: 'Blog',
+        href: '/',
+      },
+      {
+        title: 'Careers',
+        href: '/',
+      },
+    ],
+  },
+  {
+    title: 'Connect',
+    links: [
+      {
+        title: 'Contact',
+        href: '/',
+      },
+      {
+        title: 'Newsletter',
+        href: '/',
+      },
+      {
+        title: 'LinkedIn',
+        href: '/',
+      },
+    ],
+  },
+];
