2b55df6aba5068bc58b5305a7ab7378f4d1f04f2
Jon Rutter <contact@jonrutter.com>
2021-09-01 16:53:20 -0400

    Added Footer component

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+// data
+import { links } from 'data/links';
+
+// logo
+import Logo from 'images/logo.svg';
+
+const Footer = () => {
+  return (
+    <div className="bg-footer-bg py-16 px-6 rounded-tr-xl md:py-20">
+      <footer className="container mx-auto text-center block grid grid-cols-1 justify-items-center md:grid-cols-4 md:justify-items-start md:text-left">
+        <img src={Logo} alt="Blogr" className="mb-16" />
+        {links.map((link, index) => {
+          const { title, links } = link;
+          return (
+            <ul key={index} className="text-sm mb-7">
+              <li className="text-white mb-6">{title}</li>
+              {links.map((link, index) => {
+                const { title, href } = link;
+                return (
+                  <li key={index} className="mb-2">
+                    <Link
+                      to={href}
+                      className="text-footer-text hover:underline"
+                    >
+                      {title}
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          );
+        })}
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
