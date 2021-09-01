f33f0a27e8d728b9915dc13ab7ed1978baaf287e
Jon Rutter <contact@jonrutter.com>
2021-09-01 14:36:30 -0400

    Created custom hook to respond to click events outside a component

---
+++ src/hooks/useOutsideClick.js
@@ -0,0 +1,21 @@
+import React, { useEffect } from 'react';
+
+const useOutsideClick = (ref, callback) => {
+  useEffect(() => {
+    // handler
+    function handleClick(event) {
+      if (ref.current && !ref.current.contains(event.target)) {
+        callback();
+        return;
+      }
+    }
+    // listener
+    document.addEventListener('mousedown', handleClick);
+    // unbind event listener on clean up
+    return () => {
+      document.removeEventListener('mousedown', handleClick);
+    };
+  }, [ref]);
+};
+
+export default useOutsideClick;
