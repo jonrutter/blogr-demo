8f2573548c84417a3f4d9d74502237d1ba1cca21
Jon Rutter <contact@jonrutter.com>
2021-09-01 14:35:59 -0400

    Updated Tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,46 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          light: 'hsl(13, 100%, 72%)',
+          hover: 'hsl(355, 100%, 74%)',
+          DEFAULT: 'hsl(356, 100%, 66%)',
+          dark: 'hsl(353, 100%, 62%)',
+        },
+        'dark-blue': {
+          DEFAULT: 'hsl(208, 49%, 24%)',
+          desaturated: '#3F4164',
+        },
+        'body-text': 'hsl(207, 13%, 34%)',
+        footer: {
+          bg: 'hsl(240, 10%, 16%)',
+          text: 'hsl(240, 2%, 79%)',
+        },
+        'blue-gradient': {
+          start: '#2C2D3F',
+          finish: '#3F4164',
+        },
+      },
+      fontFamily: {
+        sans: ['Overpass', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+        ubuntu: ['Ubuntu', 'sans-serif'],
+        overpass: ['Overpass', 'sans-serif'],
+      },
+      backgroundImage: {
+        'hero-pattern':
+          "url('images/bg-pattern-intro.svg'), linear-gradient(to right, hsl(13, 100%, 72%), hsl(353, 100%, 62%))",
+      },
+      borderRadius: {
+        xl: '100px',
+      },
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      fontWeight: ['hover'],
+    },
   },
   plugins: [],
 };
