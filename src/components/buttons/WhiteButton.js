a2c31595701178086b93aa546824de4af94d8e98
Jon Rutter <contact@jonrutter.com>
2021-09-01 20:38:42 -0400

    Darkened WhiteButton text color to improve accessibility

--- src/components/buttons/WhiteButton.js
+++ src/components/buttons/WhiteButton.js
@@ -4,7 +4,7 @@ import Button from 'components/buttons/Button';
 
 const WhiteButton = ({ children }) => {
   return (
-    <Button type="bg-white text-brand border border-white hover:bg-brand-hover hover:text-white hover:border-brand-hover">
+    <Button type="bg-white text-brand-darkest border border-white hover:bg-brand-hover hover:text-white hover:border-brand-hover">
       {children}
     </Button>
   );
