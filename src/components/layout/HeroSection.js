a2e400d560e4e9a833bbb76e4ba1a6d7438b3401
Jon Rutter <contact@jonrutter.com>
2021-09-01 20:15:24 -0400

    Improved HeroSection spacing

--- src/components/layout/HeroSection.js
+++ src/components/layout/HeroSection.js
@@ -12,15 +12,21 @@ const HeroSection = () => {
     <div className="text-center bg-hero-pattern bg-no-repeat bg-center pt-12 px-6 pb-36 rounded-bl-xl md:pb-40 md:pt-16 md:px-8">
       <header>
         <Nav />
-        <h1 className="text-white text-4xl md:text-6xl mb-4">
-          A modern publishing platform
-        </h1>
-        <p className="text-white opacity-70 mb-10">
-          Grow your audience and build your online brand
-        </p>
-        <div className="space-x-2">
-          <WhiteButton>Start for Free</WhiteButton>
-          <TransparentButton>Learn More</TransparentButton>
+        <div className="w-full mx-auto md:container">
+          <h1 className="text-white text-3xl md:text-6xl mb-4 md:font-semibold">
+            A modern publishing platform
+          </h1>
+          <p className="text-white opacity-70 mb-10">
+            Grow your audience and build your online brand
+          </p>
+          <div className="flex items-center justify-center flex-wrap">
+            <div className="mr-2 mb-2">
+              <WhiteButton>Start for Free</WhiteButton>
+            </div>
+            <div className="mr-2 mb-2">
+              <TransparentButton>Learn More</TransparentButton>
+            </div>
+          </div>
         </div>
       </header>
     </div>
