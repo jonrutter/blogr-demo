8392b249fe49f63438197321c77ded4d36671fc0
Jon Rutter <contact@jonrutter.io>
2022-07-25 14:11:00 -0400

    Add button transitions

--- src/components/buttons/Button.js
+++ src/components/buttons/Button.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const Button = ({ type, children }) => {
   return (
-    <button className={`py-3 px-4 rounded-full font-ubuntu font-bold  ${type}`}>
+    <button
+      className={`py-3 px-4 rounded-full font-ubuntu font-bold  ${type} transition-all`}
+    >
       {children}
     </button>
   );
